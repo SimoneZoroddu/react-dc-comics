051a98d683d9c8b4c96bcae0ba2f021cc7ab0058
SimoneZoroddu <lenviur@gmail.com>
2026-03-06 16:27:04 +0100

    Main inside AppMain.jsx

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,44 @@
-export default function AppMain(){
+import buyDigitalComics from '/buy-comics-digital-comics.png'
+import buyMerchandiseComics from '/buy-comics-merchandise.png'
+import buySubscriptionsComics from '/buy-comics-subscriptions.png'
+import buyShopLocatorComics from '/buy-comics-shop-locator.png'
+import buyPowerVisaComics from '/buy-dc-power-visa.svg'
+
+export default function AppMain() {
 
 
-  return (
-    <main> 
-      <h1>Main</h1>
-    </main>
-  )
+    return (
+        <>
+            <main className="bg-black">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            Content goes here
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-primary">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <img src={buyDigitalComics} alt="buyDigitalComics" /> Digital Comics
+                            </div>
+                            <div className="col">
+                                <img src={buyMerchandiseComics} alt="buyDigitalComics" /> DC Merchandise
+                            </div>
+                            <div className="col">
+                                <img src={buySubscriptionsComics} alt="buyDigitalComics" /> Subscription
+                            </div>
+                            <div className="col">
+                                <img src={buyShopLocatorComics} alt="buyDigitalComics" /> Comic Shop Locator
+                            </div>
+                            <div className="col">
+                                <img src={buyPowerVisaComics} alt="buyDigitalComics" /> DC Power visa
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main >
+        </>
+    )
 }
