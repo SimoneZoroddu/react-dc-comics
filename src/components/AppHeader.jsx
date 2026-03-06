40bfc73c57cc474e97430a14fd0824ca2fab393f
SimoneZoroddu <lenviur@gmail.com>
2026-03-06 16:14:17 +0100

    Header with the items inside

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,22 @@
-export default function AppHeader(){
+import dc_logo from "/dc-logo.png"
 
 
-  return (
-    <header> 
-      <h1>Header</h1>
-    </header>
-  )
+export default function AppHeader() {
+
+
+    return (
+        <header>
+            <img src={dc_logo} alt="dcLogo" />
+            <a href="">Characters</a>
+            <a href="">Comics</a>
+            <a href="">Movies</a>
+            <a href="">TV</a>
+            <a href="">Games</a>
+            <a href="">Collectibles</a>
+            <a href="">Videos</a>
+            <a href="">Fans</a>
+            <a href="">News</a>
+            <a href="">Shop</a>
+        </header>
+    )
 }
